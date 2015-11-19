1dec1f58e029c51abf314a89325888e46cfc01ed
dave <concaveNP@gmail.com>
2015-11-19 05:41:27 -0700

    A middle of the road check-in.  Starting to refactor the details from a fragment to an activity.

---
+++ app/src/main/java/com/concavenp/nanodegree/popularmovies/MovieDetailsActivity.java
@@ -0,0 +1,39 @@
+package com.concavenp.nanodegree.popularmovies;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.movie_deatils_toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        final ImageView imageView = (ImageView) findViewById(R.id.movie_details_backdrop);
+
+        String posterURL = getResources().getString(R.string.base_url_image_retrieval) + poster;
+        Picasso.with(getContext()).load(posterURL).into(imageView);
+    }
+
+}
